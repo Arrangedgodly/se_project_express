903de1d6772277124fa507a0e4d0e02f4bc39b50
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-11-07 22:16:16 -0700

    refactor: this is super confusing

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -3,23 +3,23 @@ const ClothingItem = require('../models/clothingItem');
 module.exports.getClothingItems = (req, res) => {
   ClothingItem.find({})
     .then((items) => res.send({ clothingItems: items }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: `${err.message}` }));
 };
 
 module.exports.createClothingItem = (req, res) => {
   const {
-    name, weather, imageUrl, ownerId
+    name, weather, imageUrl
   } = req.body;
   ClothingItem.create({
-    name, weather, imageUrl, owner: ownerId
+    name, weather, imageUrl, owner: req.user
   })
     .then((item) => res.send({ clothingItem: item }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: `${err.message}` }));
 };
 
 module.exports.deleteClothingItem = (req, res) => {
   const { id } = req.body;
   ClothingItem.findByIdAndDelete({ id })
     .then(item => console.log(item))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: `${err.message}` }));
 };
