a147d05bf0b147481c15f549dba2c9ff337c5ee3
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-01-11 21:01:10 -0700

    getting very confused by central error processing, trying hard to keep working on it

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const helmet = require('helmet');
-const cors = require('cors');
-const userRouter = require('./routes/users');
-const clothingItemsRouter = require('./routes/clothingItems');
-const { createUser, login } = require('./controllers/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { celebrate, Joi, errors } = require("celebrate");
+const helmet = require("helmet");
+const cors = require("cors");
+const userRouter = require("./routes/users");
+const clothingItemsRouter = require("./routes/clothingItems");
+const { createUser, login } = require("./controllers/users");
+const { requestLogger, errorLogger } = require('./middlewares/logger'); 
 
 const app = express();
 
-const { PORT = 3001, DATABASE = 'mongodb://localhost:27017/wtwr_db' } = process.env;
+const { PORT = 3001, DATABASE = "mongodb://localhost:27017/wtwr_db" } =
+  process.env;
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
-  return helpers.error('string.uri');
+  return helpers.error("string.uri");
 };
 
 mongoose.connect(DATABASE);
-
-app.use(errors());
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'An error occurred on the server'
-        : message
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App live and listening at port ${PORT}`);
@@ -43,25 +32,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required(),
-    password: Joi.string().required()
-  })
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().custom(validateURL),
-    email: Joi.string().required(),
-    password: Joi.string().required()
-  })
-}), createUser);
+app.use(requestLogger);
 
-app.use('/users', userRouter);
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required().custom(validateURL),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser
+);
 
-app.use('/items', clothingItemsRouter);
+app.use("/users", userRouter);
 
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+app.use("/items", clothingItemsRouter);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "An error occurred on the server" : message,
+  });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
 });
