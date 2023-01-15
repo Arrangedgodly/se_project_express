439cca25c056fdde7f72b349a9f60cabdc85c68d
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-01-14 17:18:13 -0700

    refactor: attempting to fix the cors errors

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://graydonwasil.students.nomoredomainssbs.ru/', 'https://www.graydonwasil.students.nomoredomainssbs.ru/', 'http://localhost:3000'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+}));
 
 app.use(requestLogger);
 
